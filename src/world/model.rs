7a26a733065b93b23619aca36ac5c215c51f4e9e
Brian Lubeski <bclube@gmail.com>
2018-09-21 12:50:25 -0400

    Add Model trait to represent objects that rays can interact with

---
+++ src/world/model.rs
@@ -0,0 +1,14 @@
+use geometry::ray::*;
+use geometry::vec3::*;
+use hit_detection::hitable::*;
+use std::rc::*;
+use surface::material::*;
+
+pub struct ModelHitRecord {
+    pub hit_record: HitRecord,
+    pub material: Rc<Material>,
+}
+
+pub trait Model {
+    fn hit_model(&self, ray: &Ray, t_min: Dimension, t_max: Dimension) -> Option<ModelHitRecord>;
+}
