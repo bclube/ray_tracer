3a7d9cde843835e5897014e36ff2c551e401beef
Brian Lubeski <bclube@gmail.com>
2018-09-21 14:58:06 -0400

    Implement metal surface scattering

--- src/surface/metal.rs
+++ src/surface/metal.rs
@@ -4,15 +4,30 @@ use geometry::vec3::*;
 use surface::material::*;
 
 pub struct Metal {
-    pub albedo: ColorSample,
+    albedo: ColorSample,
+    fuzz: Dimension,
+}
+
+impl Metal {
+    pub fn new(albedo: ColorSample, fuzz: Dimension) -> Metal {
+        Metal {
+            albedo: albedo,
+            fuzz: fuzz.max(0.0).min(1.0),
+        }
+    }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray: &Ray, hit_point: &Vec3, hit_normal: &Vec3) -> Option<HitResult> {
         let reflected = reflect(ray.direction.unit(), *hit_normal);
+        let direction = if self.fuzz == 0.0 {
+            reflected
+        } else {
+            reflected + self.fuzz * Vec3::random_in_unit_sphere()
+        };
         let scattered = Ray {
             origin: *hit_point,
-            direction: reflected,
+            direction: direction,
         };
         if scattered.direction.dot(*hit_normal) > 0.0 {
             Some(HitResult {
